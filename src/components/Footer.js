3775cd463bbeba4979fe61f92be9d26ba39dfd4b
Skander <msalmi.skander@esprit.tn>
2023-02-13 00:57:47 +0100

    fix page content with list card of recette

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
+import styles from './Footer.module.scss';
+
 function Footer() {
-    return <div className="b3">Footer</div>;
+    return (
+    <footer className={`${styles.footer} d-flex flex-row align-items-center justify-content-center p-20`}>
+        <p> Copyright <i className="fa-regular fa-copyright"></i>  2023 Cookchef Skander, Inc.</p>
+    </footer>
+    );
 }
 
 export default Footer;
